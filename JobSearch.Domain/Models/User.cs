fd42fdbac015984c7b30422239a757b88fa96875
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-01-24 18:37:11 -0300

    Criando modelo de banco

---
+++ JobSearch.Domain/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace JobSearch.Domain.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+    }
+}

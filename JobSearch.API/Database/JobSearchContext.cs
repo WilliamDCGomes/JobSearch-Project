28a769d41086855e020cfbeee7bd57459bb1e16d
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-01-24 18:50:17 -0300

    Configurando banco

--- JobSearch.API/Database/JobSearchContext.cs
+++ JobSearch.API/Database/JobSearchContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using JobSearch.Domain.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace JobSearch.API.Database
 {
+    
     public class JobSearchContext : DbContext
     {
+        public JobSearchContext(DbContextOptions<JobSearchContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Job> Jobs { get; set; }
     }
 }

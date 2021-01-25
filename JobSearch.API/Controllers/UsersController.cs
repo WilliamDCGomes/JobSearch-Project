f5bd9de0f1426bca3007942e91bdf57e612493d1
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-01-25 08:52:17 -0300

    API - UserController

---
+++ JobSearch.API/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using JobSearch.API.Database;
+using JobSearch.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobSearch.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private JobSearchContext _data;
+        public UsersController(JobSearchContext data)
+        {
+            _data = data;
+        }
+        public IActionResult GetUser(string email, string password)
+        {
+            User userDb = _data.Users.FirstOrDefault(a=>a.Email == email && a.Password == password);
+            if(userDb == null)
+            {
+                return NotFound();
+            }
+            return new JsonResult(userDb);
+        }
+        [HttpPost]
+        `public IActionResult AddUser(User user)
+        {
+            _data.Users.Add(user);
+            _data.SaveChanges();
+            return CreatedAtAction(nameof(GetUser), new { email = user.Email, password = user.Password }, user);
+        }
+    }
+}

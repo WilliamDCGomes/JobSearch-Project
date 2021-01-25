ee2da8397710a1ca1b5c11172e43765788b536bf
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-01-25 20:08:32 -0300

    HttpClient - UserService

---
+++ JobSearch.App/JobSearch.App/Services/Service.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace JobSearch.App.Services
+{
+    public class Service
+    {
+        protected HttpClient _client;
+        protected string BaseApiUrl = "https://xamarinforms2020api.azurewebsites.net/";
+
+        protected Service()
+        {
+            _client = new HttpClient();
+        }
+    }
+}

700e2e55b5439ad7ba6071361b762f36b55a943f
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-01-25 20:53:07 -0300

    HttpClient - JobService

---
+++ JobSearch.App/JobSearch.App/Services/JobSevice.cs
@@ -0,0 +1,46 @@
+﻿using JobSearch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobSearch.App.Services
+{
+    public class JobSevice : Service
+    {
+        public async Task<IEnumerable<Job>> GetJobs(string word, string cityState, int numberOfPages = 1)
+        {
+            HttpResponseMessage response = await _client.GetAsync($"{BaseApiUrl}/api/Jobs?word={word}&cityState={cityState}&numberOfPage={numberOfPages}");
+            List<Job> list = null;
+            if (response.IsSuccessStatusCode)
+            {
+                list = await response.Content.ReadAsAsync<List<Job>>();
+            }
+            return list;
+        }
+        public async Task<Job> GetJob(int id)
+        {
+            HttpResponseMessage response = await _client.GetAsync($"{BaseApiUrl}/api/Jobs/{id}");
+            Job job = null;
+            if (response.IsSuccessStatusCode)
+            {
+                job = await response.Content.ReadAsAsync<Job>();
+            }
+            return job;
+        }
+        public async Task<Job> AddJob(Job job)
+        {
+            HttpResponseMessage response = await _client.PostAsJsonAsync($"{BaseApiUrl}/api/Jobs/", job);
+            if (response.IsSuccessStatusCode)
+            {
+                job = await response.Content.ReadAsAsync<Job>();
+            }
+            else
+            {
+                job = null;
+            }
+            return job;
+        }
+    }
+}
